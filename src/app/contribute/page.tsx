34b55a7ea91329c2eeed46d706e690e05ac1768a
Dave Mostoller <dmostoller@gmail.com>
2024-11-08 14:29:40 -0500

    fix: github icon

--- src/app/contribute/page.tsx
+++ src/app/contribute/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { Suspense } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import PrimaryButton from "../../components/PrimaryButton";
 import CopyablePre from "../../components/CopyablePre";
 import LoadingSpinner from "@/components/LoadingSpinner";
@@ -15,13 +14,17 @@ const ContributePageContent = () => {
         </h1>
         <div className="absolute top-0 right-0 mt-4 mr-4 md:mt-6 md:mr-6">
           <Link href="https://github.com/dmostoller/philareact" target="_blank" rel="noopener noreferrer">
-            <Image
-              src="/github-mark-white.png"
-              alt="GitHub Logo"
-              width={96}
-              height={96}
-              className="inline-block w-10 h-10 md:w-12 md:h-12 hover:scale-105 transition-transform duration-300 text-dark-slate-950"
-            />
+            <div>
+              <svg
+                width={48}
+                height={48}
+                viewBox="0 0 98 96"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-10 h-10 md:w-12 md:h-12 hover:scale-105 transition-transform duration-300 fill-dark-slate-50 dark:fill-dark-slate-50"
+              >
+                <path d="M48.9,0C21.8,0,0,22,0,49.2C0,71,14,89.4,33.4,95.9c2.4,0.5,3.3-1.1,3.3-2.4c0-1.1-0.1-5.1-0.1-9.1 c-13.6,2.9-16.4-5.9-16.4-5.9c-2.2-5.7-5.4-7.2-5.4-7.2c-4.4-3,0.3-3,0.3-3c4.9,0.3,7.5,5.1,7.5,5.1c4.4,7.5,11.4,5.4,14.2,4.1 c0.4-3.2,1.7-5.4,3.1-6.6c-10.8-1.1-22.2-5.4-22.2-24.3c0-5.4,1.9-9.8,5-13.2c-0.5-1.2-2.2-6.3,0.5-13c0,0,4.1-1.3,13.4,5.1 c3.9-1.1,8.1-1.6,12.2-1.6s8.3,0.6,12.2,1.6c9.3-6.4,13.4-5.1,13.4-5.1c2.7,6.8,1,11.8,0.5,13c3.2,3.4,5,7.8,5,13.2 c0,18.9-11.4,23.1-22.3,24.3c1.8,1.5,3.3,4.5,3.3,9.1c0,6.6-0.1,11.9-0.1,13.5c0,1.3,0.9,2.9,3.3,2.4C83.6,89.4,97.6,71,97.6,49.2 C97.7,22,75.8,0,48.9,0z" />
+              </svg>
+            </div>
           </Link>
         </div>
         <p className="mb-4 md:mb-6 text-sm md:text-base text-center">
