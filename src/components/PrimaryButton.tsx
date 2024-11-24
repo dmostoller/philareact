7ee20360d7b667e11204f6cab1748c435fa9c9c3
Dave Mostoller <dmostoller@gmail.com>
2024-11-23 23:35:26 -0500

    fix: primary button props type

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import LoadingButtonSpinner from './LoadingButtonSpinner';
 
-interface PrimaryButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+type ButtonProps = Pick<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  'onClick' | 'type' | 'disabled' | 'aria-label' | 'name' | 'form' | 'value'
+>;
+
+interface PrimaryButtonProps extends ButtonProps {
   loading?: boolean;
   className?: string;
+  children?: React.ReactNode;
 }
 
-const PrimaryButton: React.FC<PrimaryButtonProps> = ({ loading, className, children, ...props }) => {
+const PrimaryButton: React.FC<PrimaryButtonProps> = ({
+  loading,
+  className,
+  children,
+  disabled,
+  type = 'submit',
+  ...buttonProps
+}) => {
   return (
     <button
-      type="submit"
+      type={type}
       className={`px-4 py-2 font-semibold border border-dark-slate-700 bg-gradient-to-b from-dark-slate-500 to-dark-slate-700 text-dark-slate-50 rounded-lg hover:from-dark-slate-400 hover:to-dark-slate-600 ${className}`}
-      disabled={loading}
-      {...props}
+      disabled={loading || disabled}
+      {...buttonProps}
     >
       {loading ? <LoadingButtonSpinner /> : children}
     </button>
